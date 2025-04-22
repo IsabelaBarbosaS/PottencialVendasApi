968f68d8055f08f3ce865bf5eaa018d47791e9ab
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-22 17:59:42 -0300

    Add files via upload

--- Domain/Interfaces/IVendaRepository.cs
+++ Domain/Interfaces/IVendaRepository.cs
@@ -1,1 +1,14 @@
+using System;
+using Pottencial.VendasApi.Domain;
 
+namespace Pottencial.VendasApi.Repositories
+{
+    public interface IVendaRepository
+    {
+        void Adicionar(Venda venda);
+        Venda? ObterPorId(Guid id);
+        void Atualizar(Venda venda);
+        List<Venda> ObterTodas();
+
+    }
+}
