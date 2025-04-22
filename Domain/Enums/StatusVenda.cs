096a8098fee956af9c2361debc5e3740fbb0ea59
Isabela Barbosa <55892005+IsabelaBarbosaS@users.noreply.github.com>
2025-04-22 17:56:30 -0300

    Add files via upload

--- Domain/Enums/StatusVenda.cs
+++ Domain/Enums/StatusVenda.cs
@@ -1,1 +1,11 @@
-
+namespace Pottencial.VendasApi.Domain
+{
+    public enum StatusVenda
+    {
+        AguardandoPagamento,
+        PagamentoAprovado,
+        EnviadoParaTransportadora,
+        Entregue,
+        Cancelada
+    }
+}
